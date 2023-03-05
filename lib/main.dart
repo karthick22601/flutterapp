bc830e4dd2730ac2a2116a41cc0b3a9346882945
Arun pandian <arun07160@gmail.com>
2023-03-05 15:28:30 +0530

    add picture

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,37 @@
-import 'package:flutter/material.dart';  
-  
-void main() => runApp(MyApp());  
-  
-class MyApp extends StatefulWidget {  
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  @override  
-  Widget build(BuildContext context) {  
-    return MaterialApp(  
-      home: Scaffold(  
-        appBar: AppBar(  
-            title: Text('Flutter Image Demo'),  
-        ),  
-        body: SingleChildScrollView(  
-          child: Column(  
-            children: <Widget>[  
-              Image.asset('images/1.jpg'),  
-               Image.asset('images/1.jpg'),  
-                Image.asset('images/1.jpg'),  
-                              Image.asset('images/1.jpg'),  
-              Image.asset('images/1.jpg'),  
-              Image.asset('images/1.jpg'),  
-              
-              Image.asset('images/1.jpg'),  
-
-
-             /* Text(  
-                  'A tablet is a wireless touch screen computer that is smaller than a notebook but larger than a smartphone.',  
-                  style: TextStyle(fontSize: 20.0),)  */
-                
-            ],  
-          ),  
-        ),  
-      ),  
-    );  
-  }  
-}  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Image Demo'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('images/1.jpg'),
+              Image.asset('images/1.jpg'),
+              Image.asset('images/1.jpg'),
+              Image.asset('images/1.jpg'),
+              Image.asset('images/1.jpg'),
+              Image.asset('images/1.jpg'),
+              Text(
+                'A tablet is a wireless touch screen computer that is smaller than a notebook but larger than a smartphone.',
+                style: TextStyle(fontSize: 20.0),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
